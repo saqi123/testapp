07e48fbc83821ef1f18fc626b759e29b8ab89070
Nitin Singh <nitins@thoughtworks.com>
2011-02-21 06:58:03 +0530

    scaffold requests

---
+++ db/migrate/20110221012457_create_requests.rb
@@ -0,0 +1,17 @@
+class CreateRequests < ActiveRecord::Migration
+  def self.up
+    create_table :requests do |t|
+      t.string :type
+      t.string :details
+      t.string :comments
+      t.string :client
+      t.string :status
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :requests
+  end
+end
