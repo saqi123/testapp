95b9681e33e385486cb98d41d25adcceb3f41cab
Nitin Singh <nitins@thoughtworks.com>
2011-02-21 07:22:20 +0530

    1

--- db/migrate/20110221013542_create_requests.rb
+++ db/migrate/20110221013542_create_requests.rb
@@ -2,7 +2,7 @@ class CreateRequests < ActiveRecord::Migration
   def self.up
     create_table :requests do |t|
       t.string :type
-      t.string :details
+      t.text :details
       t.string :comments
       t.string :client
       t.string :status
